094ba0a80dc2af881a3ec5e3f29f3d5df8c95ca4
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-14 22:06:45 -0300

    Primeiro Commit

---
+++ app/src/main/java/CintiaAngelo/com/github/jetpackcompose/MainActivity.kt
@@ -0,0 +1,48 @@
+package CintiaAngelo.com.github.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import CintiaAngelo.com.github.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    TextoCustomizado(
+                        "Primeira função composable",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun TextoCustomizado(texto: String, modifier: Modifier = Modifier) {
+        Text(
+            text = texto,
+            modifier = modifier.fillMaxSize()
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun Preview() {
+        JetpackComposeTheme {
+            TextoCustomizado("Primeira função composable")
+        }
+    }
+}
+
